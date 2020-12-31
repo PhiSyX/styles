c12f7dadba11f8e987c2b8f734e0bdf7478f083c
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2020-12-31 18:12:58 +0100

    Retrait du dossier de distribution

---
+++ playground/vite.config.ts
@@ -0,0 +1,9 @@
+import { UserConfig as ViteUserConfig } from "vite";
+
+const config: ViteUserConfig = {
+  outDir: "../docs",
+  assetsDir: "assets",
+  sourcemap: false,
+};
+
+export default config;
