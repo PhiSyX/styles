496635bb3e774068d02f0e8b43d99796d32c6dff
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2021-01-10 01:20:56 +0100

    CSS invalide > valide (js)

--- playground/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { UserConfig as ViteUserConfig } from "vite";
-
-const config: ViteUserConfig = {
-  outDir: "../docs",
-  assetsDir: "assets",
-  sourcemap: false,
-};
-
-export default config;
